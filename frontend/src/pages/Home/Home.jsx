62847342b581b56c32546883727d5de1508089e5
Akash <therealakash13>
2024-04-29 19:34:36 +0530

    explore menu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+  const [category,setCategory] = useState("All");
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
   )
 }
 
