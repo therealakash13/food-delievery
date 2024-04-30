6ab84960434a1d27f14bb8d87d08091a511a0f5a
Akash <therealakash13>
2024-04-30 15:16:08 +0530

    app download

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import AppDownload from "./components/AppDownload/AppDownload";
 
 const App = () => {
   return (
---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+export default function AppDownload() {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Tomato App{" "}
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="play-store" />
+        <img src={assets.app_store} alt="app-store" />
+      </div>
+    </div>
+  );
+}
