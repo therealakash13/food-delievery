6bf14f1624f6d61e8dc0b21ca8b268301e150333
Akash <therealakash13>
2024-05-02 14:23:28 +0530

    place order responsive

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { food_list, cartItem, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -50,15 +53,19 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>$ 02</p>
+              <p>$ {getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Total</p>
-              <p>$ {getTotalCartAmount() + 2}</p>
+              <p>
+                $ {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </p>
             </div>
           </div>
-          <button>Proceed To Checkout</button>
+          <button onClick={() => navigate("/order")}>
+            Proceed To Checkout
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
