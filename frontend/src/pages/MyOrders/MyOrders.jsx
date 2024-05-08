2375eb9ed5f020cede2032b28001ab7975c3c86f
Akash <therealakash13>
2024-05-08 15:57:35 +0530

    order and payment functionality

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from "react";
+import { StoreContext } from "../../context/StoreContext.jsx";
+import "./MyOrders.css";
+import axios from "axios";
+import { assets } from "../../assets/assets.js";
+
+const MyOrders = () => {
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
+
+  const fetchOrder = async () => {
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      {
+        headers: { token },
+      }
+    );
+    setData(response.data.data);
+    console.log(response.data.data);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchOrder();
+    }
+  }, [token]);
+
+  return (
+    <div className="my-orders">
+      <h2>My Orders</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="Orders" />
+              <p>
+                {order.items.map((items, index) => {
+                  if (index === order.items.length - 1) {
+                    return items.name + " ( " + items.quantity + " ) ";
+                  } else {
+                    return " " + items.name + " ( " + items.quantity + " ), ";
+                  }
+                })}
+              </p>
+              <p>${order.amount}.00</p>
+              <p>Items: {order.items.length}</p>
+              <p className="status">{order.status}</p>
+              <button>Track Order</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
